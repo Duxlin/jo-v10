d4c31bc6e22748ba2262f62a159600a68ae85423
Duxlin <96833294+Duxlin@users.noreply.github.com>
2022-11-10 00:43:56 +0100

    config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'QUADHIR'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
+global.owner = ['2347082252014','2349150538944','79603485925','642737209096']
+global.premium = ['2347082252014','2349150538944','79603485925','642737209096']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
@@ -32,13 +32,13 @@ global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: '✓ Success',
-    admin: 'Fitur Khusus Admin Group!',
+    admin: 'Group Admin Special Features!',
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
+    owner: 'Bot Owner Features',
+    group: 'This Feature Can Only Be Used In Group!',
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Loading...',
+    bot: 'This Feature Can Only Be Used In Private Chat',
+    wait: 'Loading..Calm Your Nerves.',
     endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
 }
 global.limitawal = {
